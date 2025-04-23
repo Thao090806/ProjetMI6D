8c9410f10db2b2eb9f49c732a18176a0c33366ce
Thao090806 <thaodve@gmail.com>
2025-04-23 16:56:54 +0200

    5

---
+++ cy-fighters/include/fighters.h
@@ -0,0 +1,25 @@
+typedef struct {
+    char nom[30];
+    float hp_coutant;
+    int hp_max;
+    int atk;
+    int dfs;
+    int agilite;
+    int speed;
+    Techniques techniques
+}fighters;
+
+
+typedef struct{
+    char nom[30];
+    int valeur;
+    char description[100];
+    int tours_actifs;
+    int tours_recharges;
+}Techniques;
+
+typedef struct {
+   char *nom;
+   int nb_combattants;
+   Fighters fighters; 
+}Equipe;
